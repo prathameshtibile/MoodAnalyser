2101a4d32ffb345a8f9027f80d15eea74a78cb5f
Prathamesh Tibile <prathameshtibile1997@gmail.com>
2021-07-01 14:35:52 +0530

    UC2 test file added

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -6,37 +6,49 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
-	 //UC1-TC1.1
+	 //UC1-TC1.1 Add
     @Test
-    public void givenIAmInSadMood_shouldReturnSad() {
+    public  void givenIAmInSadMood_shouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals(mood, "SAD");
     }
 
-    //UC1-TC1.2
+    //UC1-TC1.2 Add
     @Test
-   public void givenIAmInAnyMood_shouldReturnHappy() {
+   public void givenIAmInAnyMood_shouldReturnHappy()
+    {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any mood");
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals(mood, "HAPPY");
     }
 
-    //UC1-TC-1.1 Repeat
+    //UC1-TC-1.1 Repeat Add
     @Test
-    public void givenIAmInSadMood_shouldReturnsad() {
+    public void givenIAmInSadMood_shouldReturnHappy() 
+    {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals(mood, "SAD");
     }
-    //UC1-TC-1.2 Repeat
+    
+    //UC1-TC-1.2 Repeat Add
     @Test
-    public void givenIAmInHappyMood_shouldReturnHappy() {
+    public void givenIAmInHappyMood_shouldReturnHappy()
+    {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals(mood, "HAPPY");
     }
-	
+
+    //UC2.1 - given Null exception return HAPPY Add
+    @Test
+    public void givenNullException_shouldReturnHappy() 
+    {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood();
+        Assert.assertEquals(mood, "HAPPY");
+    }
 		
 	}
 
